e943fb902a70330c22cfe46e913a12efea640460
wuyue112524 <clairewu0201@gmail.com>
2020-10-22 15:55:55 +0100

    Add pre-process framework

---
+++ Pre-process/pre-processor.py
@@ -0,0 +1,17 @@
+class PreProcess:
+
+	'''input: a string
+	  return: a pre-processed string '''
+
+	def process_stop_words(self):
+		pass
+
+	def process_punctuations(self):
+		pass
+
+
+	def process_sepcial_characters(self):
+		pass
+
+	def split_words(self):
+		pass 
